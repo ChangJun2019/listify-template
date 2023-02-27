bb99167e40bde9372dff3644e695eaac93b1fad3
changjun <52chinaweb@gmail.com>
2023-02-27 15:17:00 +0800

    init

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  listify: {
+    author: 'your name',
+    icons: {
+      home: 'ph:triangle-bold',
+      dark: 'ri-moon-line',
+      system: 'mi-computer',
+      light: 'ri-sun-line',
+    },
+  }
+})
---
+++ content/index.md
@@ -0,0 +1,8 @@
+---
+title: listify
+layout: home
+---
+
+## 标题
+
+你好，这是我的一些关于 xx 的列表集合。
